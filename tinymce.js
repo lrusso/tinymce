c4015e35efba0f99b679c0ba586425ee4806ab74
Leonardo Russo <contacto@lrusso.com.ar>
2019-03-02 07:35:39 -0300

    Add files via upload

--- tinymce.js
+++ tinymce.js
@@ -17635,6 +17635,11 @@
       };
       editor.on('init', function () {
         tinymce.activeEditor.getWin().scrollTo(0,0);
+        if (editor.getBody().innerHTML.indexOf('<table')==0)
+          {
+          editor.selection.getSel().modify('move', 'forward', 'lineboundary');
+          editor.selection.getSel().modify('move', 'backward', 'lineboundary');
+          }
         self.add();
       });
       editor.on('BeforeExecCommand', function (e) {
